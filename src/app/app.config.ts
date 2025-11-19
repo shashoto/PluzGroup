c3fa0bc0926f5acfcfd83517b5c59f5e618241de
Shashoto laha <shashotolaha.altpi@gmail.com>
2025-11-19 23:37:19 +0530

    setup/complete: full setup with eslint, prettier, font and tailwind

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { primeNGConfig } from '@configs/primeng/primeng.config';
+import { routerConfig } from '@configs/router/router.config';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    routerConfig(),
+    providePrimeNG(primeNGConfig()),
+  ],
 };
